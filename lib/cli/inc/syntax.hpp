b2696bcf7b785efb52bbcffddb519e6399bae0d2
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-04 15:03:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/cli/inc/syntax.hpp
+++ lib/cli/inc/syntax.hpp
@@ -3,16 +3,26 @@
 #include "libc.hpp"
 #include "core.hpp"
 
-extern int yylex();
-extern int yylineno;
-extern char *yytext;
-extern char *yyfile;
-extern FILE *yyin;
-extern int yyparse();
-extern void yyerror(char *msg);
+/// @defgroup syntax syntax
+/// @ingroup cli
+/// @brief parser interface
+/// @{
+
+extern int yylex();              ///< lexer (`flex`)
+extern int yylineno;             ///< current line
+extern char *yytext;             ///< lexeme value
+extern char *yyfile;             ///< current file name
+extern FILE *yyin;               ///< current file handler
+extern int yyparse();            ///< parser (`bison`)
+extern void yyerror(char *msg);  ///< syntax error callback
 #include "parser.yacc.hpp"
+/// @brief construct token `(Class,ID)`
+/// @param[in] class name: calls `C(char*)` constructor
+/// @param[in] .yacc token identifier
 #define TOKEN(C, X)               \
     {                             \
         yylval.o = new C(yytext); \
         return X;                 \
     }
+
+/// @}
