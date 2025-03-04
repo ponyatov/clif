a7cb73c028748e3cc0fd411a0a7a6e3bda83d097
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-04 11:08:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/syntax.cmake
+++ cmake/syntax.cmake
@@ -6,43 +6,43 @@ set(RAGEL_EXECUTABLE_opts -C -G2 )
 
 # foreach(RAGEL_FILE ${RL})
 #     string(REGEX REPLACE ".+\/(.+)\.ragel$" "${CMAKE_CURRENT_SOURCE_DIR}/\\1.ragel.cpp"
-#                             PARSER_FILE ${RAGEL_FILE})
-#     list(APPEND CP          ${PARSER_FILE})
+#                             PARSER_CPP ${RAGEL_FILE})
+#     list(APPEND CP          ${PARSER_CPP})
 #     add_custom_command(
-#         OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_FILE}
+#         OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_CPP}
 #         DEPENDS             ${RAGEL_FILE}
 #         WORKING_DIRECTORY   ${CMAKE_SOURCE_DIR}
 #         COMMAND             ${RAGEL_EXECUTABLE}
-#         ARGS                ${RAGEL_EXECUTABLE_opts} -o ${PARSER_FILE} ${RAGEL_FILE}
+#         ARGS                ${RAGEL_EXECUTABLE_opts} -o ${PARSER_CPP} ${RAGEL_FILE}
 #     )
 # endforeach()
 
 # foreach(LEX_FILE ${L})
 #     string(REGEX REPLACE ".+\/(.+)\.lex$" "tmp/\\1.lex.cpp"
-#                             PARSER_FILE ${LEX_FILE})
-#     list(APPEND CP          ${PARSER_FILE})
+#                             PARSER_CPP ${LEX_FILE})
+#     list(APPEND CP          ${PARSER_CPP})
 #     add_custom_command(
-#         OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_FILE}
+#         OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_CPP}
 #         DEPENDS             ${LEX_FILE}
 #         WORKING_DIRECTORY   ${CMAKE_SOURCE_DIR}
 #         COMMAND             ${FLEX_EXECUTABLE}
-#         ARGS                -o ${PARSER_FILE} ${LEX_FILE}
+#         ARGS                -o ${PARSER_CPP} ${LEX_FILE}
 #     )
 # endforeach()
 
 foreach(YACC_FILE ${Y})
-    cmake_path(REPLACE_EXTENSION
-        YACC_FILE LAST_ONLY ".yacc.cpp"
-        OUTPUT_VARIABLE PARSER_FILE)
-    message("-- | " ${YACC_FILE} " " ${PARSER_FILE})
-#     string(REGEX REPLACE ".+\/(.+)\.yacc$" "${CMAKE_CURRENT_SOURCE_DIR}/\\1.yacc.cpp"
-#                             PARSER_FILE ${YACC_FILE})
-#     list(APPEND CP          ${PARSER_FILE})
-#     add_custom_command(
-#         OUTPUT              ${CMAKE_SOURCE_DIR}/${PARSER_FILE}
-#         DEPENDS             ${YACC_FILE}
-#         WORKING_DIRECTORY   ${CMAKE_SOURCE_DIR}
-#         COMMAND             ${BISON_EXECUTABLE}
-#         ARGS                -o ${PARSER_FILE} ${YACC_FILE}
-#     )
+    string(REGEX REPLACE ".+\/(.+)\.yacc$" "${CMAKE_BINARY_DIR}/\\1.yacc.cpp"
+        PARSER_CPP ${YACC_FILE})
+    string(REGEX REPLACE ".+\/(.+)\.yacc$" "${CMAKE_BINARY_DIR}/\\1.yacc.hpp"
+        PARSER_HPP ${YACC_FILE})
+    message("-- | " ${YACC_FILE} " " ${PARSER_CPP})
+    list(APPEND CP          ${PARSER_CPP})
+    list(APPEND HP          ${PARSER_HPP})
+    add_custom_command(
+        OUTPUT              ${PARSER_CPP} ${PARSER_HPP}
+        DEPENDS             ${YACC_FILE}
+        WORKING_DIRECTORY   ${CMAKE_SOURCE_DIR}
+        COMMAND             ${BISON_EXECUTABLE}
+        ARGS                -o ${PARSER_CPP} ${YACC_FILE}
+    )
 endforeach()
