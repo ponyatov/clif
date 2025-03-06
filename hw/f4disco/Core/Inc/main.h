0a2c4e741b10d3541c1b67d5cbdea1b59061870f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-06 16:50:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/f4disco/Core/Inc/main.h
+++ hw/f4disco/Core/Inc/main.h
@@ -57,6 +57,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define HSE_IN_Pin GPIO_PIN_0
+#define HSE_IN_GPIO_Port GPIOH
+#define HSE_OUT_Pin GPIO_PIN_1
+#define HSE_OUT_GPIO_Port GPIOH
 #define SWDIO_Pin GPIO_PIN_13
 #define SWDIO_GPIO_Port GPIOA
 #define SWCLK_Pin GPIO_PIN_14
