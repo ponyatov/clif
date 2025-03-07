844b7dc29dc9319ecdd443049c869c2a1e7a91a8
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-07 17:57:04 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ os/bare/bare.cmake
@@ -0,0 +1,3 @@
+add_compile_definitions(
+    BARE
+)
--- os/inc/main.hpp
+++ os/inc/main.hpp
@@ -1,4 +1,17 @@
 #pragma once
 
+#ifndef BARE
 extern int main(int argc, char *argv[]);
-extern void arg(int argc, char *argv);
+#endif  // BARE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void arg(int argc, char *argv);  ///< print boot/command line argument
+extern void setup(void);                ///< 
+extern void loop(void);                 ///<
+
+#ifdef __cplusplus
+}
+#endif
