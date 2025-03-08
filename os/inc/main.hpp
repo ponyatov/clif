2bfba4a7f15ce77bf40eecb8d50def06556a8b94
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-08 12:09:30 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- os/inc/main.hpp
+++ os/inc/main.hpp
@@ -9,7 +9,7 @@ extern "C" {
 #endif
 
 extern void arg(int argc, char *argv);  ///< print boot/command line argument
-extern void setup(void);                ///< 
+extern void setup(void);                ///<
 extern void loop(void);                 ///<
 
 #ifdef __cplusplus
