268df10e894f8737c0bb6d37ad3d113c05bb68ae
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-10 17:54:49 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/clif.cpp
+++ src/clif.cpp
@@ -10,18 +10,19 @@ int n = 0;
 extern "C" int __io_putchar(int ch) {
     uint8_t c = ch;
     CDC_Transmit_FS(&c, 1);
+    ITM_SendChar(ch);
     return ch;
 }
 
 extern "C" int _write(int file, char *ptr, int len) {
     CDC_Transmit_FS((uint8_t *)ptr, len);
+    for (int i = 0; i < len; i++) ITM_SendChar(*ptr++);
     return len;
 }
 
 extern "C" _ssize_t _write_r(struct _reent *ptr, int fd, const void *buf,
                              size_t cnt) {
-    CDC_Transmit_FS((uint8_t *)ptr, cnt);
-    return (_ssize_t)cnt;
+    return _write(fd,(char*)buf,cnt);
 }
 
 extern "C" void loop(void) {
