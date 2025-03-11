08c4f0cb4cf18e46bb02608dd6da37d98dd392e9
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-11 13:05:09 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/clif.cpp
+++ src/clif.cpp
@@ -5,8 +5,8 @@ extern "C" void setup(void) {
     // printf("Hello");
 }
 
-uint8_t buf[0x100];
-int n = 0;
+// uint8_t buf[0x100];
+// int n = 0;
 
 extern "C" int __io_putchar(int ch) {
     uint8_t c = ch;
@@ -21,18 +21,12 @@ extern "C" int _write(int file, char *ptr, int len) {
     return len;
 }
 
-// extern "C" _ssize_t _write_r(struct _reent *ptr, int fd, const void *buf,
-//                              size_t cnt) {
-//     return _write(fd,(char*)buf,cnt);
-// }
-
-    char c = 0;
+char c = 0;
 extern "C" void loop(void) {
-    sprintf((char *)buf, "Hello, %i\n", n++);
-    CDC_Transmit_FS(buf, strlen((char *)buf));
+    // sprintf((char *)buf, "Hello, %i\n", n++);
+    // CDC_Transmit_FS(buf, strlen((char *)buf));
     // printf("Hello, %i\n", n++);
     // __io_putchar(c++);
-    // putchar(c++);
-    // __io_putchar('A');
+    putchar(c++);
     HAL_Delay(22);
 }
